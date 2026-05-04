05e94342b23b63e52c9d8f5097d3c23517cb1ead
Muhammad Zohaib Malik <muhammadzohaibmalik10@gmail.com>
2026-05-04 12:42:36 +0500

    feat: implement message deletion functionality with real-time socket updates

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { sendMessage, getMessages } from "../controllers/message.controller.js";
+import { sendMessage, getMessages, deleteMessage } from "../controllers/message.controller.js";
 import { verifyAccessToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/:receiverId", verifyAccessToken, getMessages);
 router.post("/send/:receiverId", verifyAccessToken, sendMessage);
-
+router.delete("/:messageId", verifyAccessToken, deleteMessage);
 
 export default router;
