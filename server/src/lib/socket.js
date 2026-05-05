3d5a8b0c467120c6024b1f5d35f9ba11742e07a4
Muhammad Zohaib Malik <muhammadzohaibmalik10@gmail.com>
2026-05-06 01:00:12 +0500

    feat: initialize socket.io server and user connection tracking map

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -1,8 +1,6 @@
 import { Server } from "socket.io"
 import http from "http"
 import express from 'express'
-import { CLIENT_RENEG_LIMIT } from "tls"
-
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
